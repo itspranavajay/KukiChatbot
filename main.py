bb866bec617f1e373e3bf409552e6f82858943b8
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-09-26 09:33:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ from pyrogram import filters, Client
 import requests
 import os
 
+API_ID = os.environ.get("API_ID", None) 
+API_HASH = os.environ.get("API_HASH", None) 
+TOKEN = os.environ.get("TOKEN", None) 
+
+
 kuki = Client(
-    "kukibot",
-    api_id=os.environ.get("API_ID")
-    api_hash=os.environ.get("API_HASH"),
-    bot_token=os.environ.get("TOKEN")
+      bot_token=TOKEN,
+      api_id=API_ID,
+      api_hash=API_HASH,
 )
 
 @kuki.on_message(
